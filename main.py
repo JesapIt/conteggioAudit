d53d1f256ef12736528c2450006a438d30b3d56b
JesapIt <119874282+JesapIt@users.noreply.github.com>
2023-02-26 21:28:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def fun():
 nome = st.text_input('Nome e/o Cognome')
 data = st.date_input('Data', value=date.today())
 options = ['Call d\'area', 'Assemblea mensile', 'Delega', 'Recruiting', 'Mentoring', 'Progetto esterno'
-,'Progetto interno', 'Formazione', 'Call HR buddy', 'Organizzazione area', 'Task interno', 'Evento']
+,'Progetto interno', 'Formazione', 'Call HR buddy', 'Organizzazione area', 'Task interno', 'Evento', 'Report d\'area', 'Altri report', 'Auditing progetto', 'PowerBI']
 
 att = st.multiselect('Attività', options, key="multi")
 dictionary = {}
